4f5b352a413c607316397c7f1f54225fdd7fc46e
Jason Viola <jason.viola@mavs.uta.edu>
2019-08-04 21:55:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-:grace_hopper
+return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     return :alan_kay[:known_for]
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
